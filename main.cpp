b4fe1379eba5c2fe7d93b99ecd3bff3434d162d6
Megan De Leon <megandeleon@weber.edu>
2026-05-05 20:44:07 -0600

    Add TODO for employee vector and memory management
    Add TODO comments for employee management and memory handling

---
+++ main.cpp
@@ -0,0 +1,6 @@
+/*TODO
+Crear un std::vector<Employee*> para almacenar punteros a empleados.
+Instanciar objetos de ambos tipos de empleados dinámicamente (new).
+Recorrer el vector utilizando un bucle para invocar el método calculatePay() de forma polimórfica.
+Importante: Liberar la memoria de cada objeto al finalizar el programa para evitar fugas de memoria (memory leaks)
+*/
